23ad65c3e3dbb1815d77e3bfde4bd92f618acbdf
Misha Smirnov <mikeusru@gmail.com>
2017-12-20 15:16:09 -0500

    Update IO API and set functions for matlab interface

--- interfaces/matlab/eventhandlerChanged.m
+++ interfaces/matlab/eventhandlerChanged.m
@@ -1,14 +1,14 @@
 %Once a change has been detected in the text file, this function runs
 %put code to read text file here
 function eventhandlerChanged(source,arg)
-% global test
-% test.source = source
-% test.arg = arg
-disp('TXT file changed')
+%%This function runs every time the text file is changed
+% disp('TXT file changed')
 readNewInstructions(arg.FullPath.char)
 end
 
 function readNewInstructions(fullpath)
+%%This functions reads the text file and adds new lines to the allCommands
+%%variable, and to the commandQueue
 global allCommands commandQueue
 fid = fopen(fullpath,'r');
 ii = 0;
@@ -32,25 +32,107 @@ translateNewCommands(); %this runs here so program doesn't wait to close file be
 end
 
 function translateNewCommands()
+%%This function takes commands out of the commandQueue variable and sends them
+%to their appropriate functions
 global commandQueue
 while ~isempty(commandQueue)
     command = commandQueue{1};
     commandQueue(1)=[];
+    argCount = length(command) - 1;
     switch lower(command{1})
         case 'movexyz'
             disp('movexyz')
+            if checkArgCount([3,3],argCount)
+                continue
+            end
+            x = str2double(command(2));
+            y = str2double(command(3));
+            z = str2double(command(4));
+            moveXYZ(x,y,z);
         case 'grabonestack'
             disp('grabonestack')
+            if checkArgCount([0,0],argCount)
+                continue
+            end
+            grabOneStack();
         case 'setzoom'
             disp('setzoom');
+            if checkArgCount([1,1],argCount)
+                continue
+            end
+            zoom = command(2);
+            setZoom(zoom);
         case 'rununcaging'
             disp('rununcaging')
+            if checkArgCount([0,0],argCount)
+                continue
+            end
+            runUncaging();
         case 'getcurrentposition'
             disp('getcurrentposition')
+            if checkArgCount([0,0],argCount)
+                continue
+            end
+            getCurrentPosition();
         case 'getfov_xy'
             disp('getfov_xy')
+            if checkArgCount([0,0],argCount)
+                continue
+            end
+            getFOV_xy();
         otherwise
             disp('COMMAND NOT UNDERSTOOD')
     end
 end
+end
+
+function status = checkArgCount(low_high, amount)
+% return status = 0 if correct number of arguments, otherwise return 1
+if amount < low_high(1) || amount > low_high(2)
+    status = 1;
+else
+    status = 0;
+end
+if status == 1
+    disp('WRONG NUMBER OF ARGUMENTS READ');
+end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Functions sent to imaging program
+
+function moveXYZ(x,y,z)
+% move to new XYZ position
+
+% respond with StageMoveDone
+end
+
+function grabOneStack()
+% Grab an image stack
+
+% respond with GrabOneStackDone
+end
+
+function setZoom(zoom)
+% set zoom to new value
+
+% respond with Zoom
+end
+
+function runUncaging()
+% initiate uncaging
+
+% respond with UncagingDone
+end
+
+function getCurrentPositions()
+% request for current xyz position
+
+% respond with CurrentPosition
+end
+
+function getFOV_xy()
+% request FOV size in µm, in x and y
+
+% respond with fov_XY_um
 end
