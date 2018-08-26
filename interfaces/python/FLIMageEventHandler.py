9ce4323b6b37457cdf81d17f5691c6dd234f8154
smirnovm <mikeusru@gmail.com>
2018-08-26 10:46:45 -0400

    Create function placeholders for Ryohei in FLIMageEventHandler

--- interfaces/python/FLIMageEventHandler.py
+++ interfaces/python/FLIMageEventHandler.py
@@ -1,7 +1,7 @@
 import os
 from queue import Queue
 
-import numpy as np
+from interfaces.python.WriterToSpineTracker import WriterToSpineTracker
 
 
 class FLIMageEventHandler:
@@ -47,17 +47,7 @@ class FLIMageEventHandler:
     def interpret_line(self, line):
 
         self.command_interpreter.interpret(line)
-
-        def check_num_args(total_args, min_args, max_args):
-            if total_args is None:
-                len_args = 0
-            else:
-                len_args = len(total_args)
-            if min_args <= len_args <= max_args:
-                return True
-            else:
-                print(f'Error - Missing arguments. Expected between {min_args} and {max_args}. Got {len_args}')
-                return False
+        self.command_interpreter.run_current_command()
 
 
 class CommandInterpreter:
@@ -66,11 +56,15 @@ class CommandInterpreter:
         self.current_command = ''
         self.current_args = []
         self.command_dict = {}
+        self.command_to_spine_tracker = WriterToSpineTracker()
 
     def interpret(self, line):
         line_parts = line.split(',')
-        self.command = line_parts[0].lower()
-        self.args = line_parts[1:]
+        self.current_command = line_parts[0].lower()
+        self.current_args = line_parts[1:]
+
+    def run_current_command(self):
+        self.command_dict[self.current_command].run(self.current_args)
 
     def initialize_commands(self):
         self.add_command('movexyz', 3, 3, self.move_x_y_z)
@@ -83,22 +77,115 @@ class CommandInterpreter:
         self.add_command('getscananglemultiplier', 0, 0, self.get_scan_angle_multiplier)
         self.add_command('getscananglerangereference', 0, 0, self.get_scan_angle_range_reference)
         self.add_command('setscananglexy', 2, 2, self.set_scan_angle_x_y)
-        self.add_command('setscananglexy', 2, 2, self.set_scan_angle_x_y)
         self.add_command('setzslicenum', 1, 1, self.set_z_slice_num)
         self.add_command('setxyresolution', 2, 2, self.set_x_y_resolution)
         self.add_command('getxyresolution', 0, 0, self.get_x_y_resolution)
 
-    def add_command(self, name, min_args, max_args, function):
-        self.command_dict[name] = SpineTrackerCommand(name, min_args, max_args, function)
+    def add_command(self, name, min_args, max_args, fxn):
+        self.command_dict[name] = SpineTrackerCommand(name, min_args, max_args, fxn)
 
-    def move_x_y_z(self):
-        pass
+    def move_x_y_z(self, args):
+        x, y, z = [float(arg) for arg in args]
+        # TODO: Move to new position
+        self.command_to_spine_tracker.send_command('StageMoveDone', x, y, z)
+
+    def grab_one_stack(self, args):
+        x, y, z = [float(arg) for arg in args]
+        # TODO: Grab Stack
+        save_path = 'path to tiff which was just saved'
+        self.command_to_spine_tracker.send_command('GrabOneStackDone', save_path)
+
+    def set_zoom(self, args):
+        zoom = float(args[0])
+        # TODO: Set Zoom
+        self.command_to_spine_tracker.send_command('Zoom', zoom)
+
+    def run_uncaging(self, args):
+        # TODO: Run Uncaging
+        self.command_to_spine_tracker.send_command('UncagingDone')
+
+    def get_current_position(self, args):
+        # TODO: Get Current MOTOR coordinates
+        x = 0
+        y = 0
+        z = 0
+        self.command_to_spine_tracker.send_command('CurrentPosition', x, y, z)
+
+    def get_fov_x_y(self, args):
+        # TODO: Get the full field of view in µm...
+        # This is not necessary and can be set in SpineTracker manually.
+        x = 250
+        y = 250
+        self.command_to_spine_tracker.send_command('fov_XY_um', x, y)
+
+    def get_scan_angle_x_y(self, args):
+        # TODO: Get Galvo Scan Angles
+        scan_shift_fast = 0
+        scan_shift_slow = 0
+        self.command_to_spine_tracker.send_command('ScanAngleXY',
+                                                   scan_shift_fast,
+                                                   scan_shift_slow)
+
+    def get_scan_angle_multiplier(self, args):
+        # TODO: Get Scan Angle Multiplier
+        scan_angle_multiplier_fast = 1
+        scan_angle_multiplier_slow = 1
+        self.command_to_spine_tracker.send_command('ScanAngleMultiplier',
+                                                   scan_angle_multiplier_fast,
+                                                   scan_angle_multiplier_slow)
+
+    def get_scan_angle_range_reference(self, args):
+        # TODO: Get Scan Angle Multiplier
+        scan_angle_range_reference_fast = 15
+        scan_angle_range_reference_slow = 15
+        self.command_to_spine_tracker.send_command('ScanAngleRangeReference',
+                                                   scan_angle_range_reference_fast,
+                                                   scan_angle_range_reference_slow)
+
+    def set_scan_angle_x_y(self, args):
+        scan_shift_fast, scan_shift_slow = [float(arg) for arg in args]
+        # TODO: Set scan angle
+        # respond with get scan angle command
+        self.get_scan_angle_x_y([scan_shift_fast, scan_shift_slow])
+
+    def set_z_slice_num(self, args):
+        num_z_slices = int(args[0])
+        # TODO: Set number of z slices
+        self.command_to_spine_tracker.send_command('ZSliceNum', num_z_slices)
+
+    def set_x_y_resolution(self, args):
+        pixels_per_line, lines_per_frame = [int(arg) for arg in args]
+        # TODO: Set imaging resolution
+        # respond with get_x_y_resolution
+        self.get_x_y_resolution(None)
+
+    def get_x_y_resolution(self, args):
+        # TODO: get imaging resolution
+        pixels_per_line = 128
+        lines_per_frame = 128
+        self.command_to_spine_tracker.send_command('x_y_resolution', pixels_per_line, lines_per_frame)
 
 
 class SpineTrackerCommand:
 
-    def __init__(self, name, min_args, max_args, function):
+    def __init__(self, name, min_args, max_args, fxn):
         self.name = name
         self.min_args = min_args
         self.max_args = max_args
-        self.function = function
+        self.function = fxn
+
+    def run(self, args):
+        if not self.check_num_args(args):
+            return
+        self.function(args)
+
+    def check_num_args(self, args):
+        if args is None:
+            len_args = 0
+        else:
+            len_args = len(args)
+        if self.min_args <= len_args <= self.max_args:
+            return True
+        else:
+            print(f'Error - Missing arguments. Expected between {self.min_args} and {self.max_args}. Got {len_args}')
+            return False
---
+++ interfaces/python/WriterToSpineTracker.py
@@ -0,0 +1,9 @@
+
+
+class WriterToSpineTracker:
+
+    def __init__(self):
+        pass
+
+    def send_command(self, *args):
+        pass
