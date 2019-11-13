bf09643eaee05f7f04ca20210980449540da4536
mikeusru <mikeusru@gmail.com>
2019-11-13 12:14:31 -0500

    reset MacroWindow to original configuration, removed session/settings from StartPage

--- guis/StartPage.py
+++ guis/StartPage.py
@@ -1,42 +1,54 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter.filedialog import asksaveasfilename
-import os
-import matplotlib
 import numpy as np
 from matplotlib import patches
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
+from io_communication.Event import initialize_events
 from utilities.helper_functions import fit_fig_to_canvas
+
 
 class StartPage(ttk.Frame):
     name = 'Main'
 
     def __init__(self, container, session):
         ttk.Frame.__init__(self, container)
-        self.session = session
-        self.settings = session.settings
+        self.container = container
         self.bind("<Visibility>", self.on_visibility)
-        self.gui = self.build_gui_items()
+        self.settings = {
+            'large_font': None,
+            'huge_font': None,
+            'fig_dpi': None
+        }
+        self.gui_vars = {
+            'drift_label': None,
+            'communication_log': None,
+            'display_timer': None
+        }
+        self.events = initialize_events([
+            'start_imaging',
+            'stop_imaging'
+        ])
+        self.shared_figs = None
+        self.gui = None
 
     def build_gui_items(self):
-        session = self.session
         gui = dict()
         gui['frame_left_buttons'] = ttk.Frame(self)
         gui['frame_left_buttons'].grid(row=0, column=0, rowspan=2, sticky='nw')
         gui['button_start'] = ttk.Button(gui['frame_left_buttons'], text="Start Imaging",
-                                         command=lambda: session.start_imaging())
+                                         command=lambda: self.events['start_imaging']())
         gui['button_start'].grid(row=3, column=0, padx=10, pady=10, sticky='nw')
         gui['button_end'] = ttk.Button(gui['frame_left_buttons'], text="Stop Imaging",
-                                       command=lambda: session.stop_imaging())
+                                       command=lambda: self.events['stop_imaging']())
         gui['button_end'].grid(row=4, column=0, padx=10, pady=10, sticky='nw')
-        gui['drift_label'] = tk.Label(self, textvariable=self.settings.get_gui_var('drift_label'),
-                                      font=session.settings.get('large_font'))
+        gui['drift_label'] = tk.Label(self, textvariable=self.gui_vars['drift_label'],
+                                      font=self.settings['large_font'])
         gui['drift_label'].grid(row=0, column=1, padx=10, pady=10, sticky='nw')
         gui['frame_for_canvases'] = ttk.Frame(self)
         gui['frame_for_canvases'].grid(row=1, column=1, columnspan=2, rowspan=2)
-        gui['figure_af_images'] = Figure(figsize=(5, 2), dpi=session.settings.get('fig_dpi'))
+        gui['figure_af_images'] = Figure(figsize=(5, 2), dpi=self.settings['fig_dpi'])
         gui['figure_af_images'].subplots_adjust(left=0, right=1, bottom=0, wspace=0.02, hspace=0)
         gui['axes_af_images'] = [gui['figure_af_images'].add_subplot(1, 1, 1)]
         gui['canvas_af'] = FigureCanvasTkAgg(gui['figure_af_images'], gui['frame_for_canvases'])
@@ -44,31 +56,31 @@ class StartPage(ttk.Frame):
         gui['canvas_af'].get_tk_widget().config(borderwidth=1, background='gray', highlightcolor='gray',
                                                 highlightbackground='gray')
         gui['canvas_af'].get_tk_widget().grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
-        gui['canvas_timeline'] = FigureCanvasTkAgg(session.gui.shared_figs['timeline_figure'],
+        gui['canvas_timeline'] = FigureCanvasTkAgg(self.shared_figs['timeline_figure'],
                                                    gui['frame_for_canvases'])
         gui['canvas_timeline'].draw()
         gui['canvas_timeline'].get_tk_widget().config(borderwidth=1, background='gray', highlightcolor='gray',
                                                       highlightbackground='gray')
         gui['canvas_timeline'].get_tk_widget().grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
-        gui['canvas_positions'] = FigureCanvasTkAgg(session.gui.shared_figs['f_positions'],
+        gui['canvas_positions'] = FigureCanvasTkAgg(self.shared_figs['f_positions'],
                                                     gui['frame_for_canvases'])
         gui['canvas_positions'].draw()
         gui['canvas_positions'].get_tk_widget().config(borderwidth=1, background='gray', highlightcolor='gray',
                                                        highlightbackground='gray')
         gui['canvas_positions'].get_tk_widget().grid(row=0, column=1, rowspan=2, padx=10, pady=10, sticky='nsew')
-        gui['communication_log_label'] = tk.Label(self, textvariable=self.settings.get_gui_var('communication_log'),
-                                                  font=session.settings.get('large_font'))
+        gui['communication_log_label'] = tk.Label(self, textvariable=self.gui_vars['communication_log'],
+                                                  font=self.settings['large_font'])
         gui['communication_log_label'].grid(row=3, column=1, columnspan=2, padx=10, pady=10, sticky='nw')
-        gui['display_timer_label'] = tk.Label(self, textvariable=self.settings.get_gui_var('display_timer'),
-                                              font=session.settings.get('huge_font'))
+        gui['display_timer_label'] = tk.Label(self, textvariable=self.gui_vars['display_timer'],
+                                              font=self.settings['huge_font'])
         gui['display_timer_label'].grid(row=3, column=0, columnspan=1, padx=10, pady=10, sticky='nw')
-        return gui
+        self.gui = gui
 
     def on_visibility(self, event):
-        fit_fig_to_canvas(self.session.gui.shared_figs['timeline_figure'], self.gui['canvas_timeline'],
-                          self.session.settings.get('fig_dpi'))
-        fit_fig_to_canvas(self.session.gui.shared_figs['f_positions'], self.gui['canvas_positions'],
-                          self.session.settings.get('fig_dpi'))
+        fit_fig_to_canvas(self.shared_figs['timeline_figure'], self.gui['canvas_timeline'],
+                          self.settings['fig_dpi'])
+        fit_fig_to_canvas(self.shared_figs['f_positions'], self.gui['canvas_positions'],
+                          self.settings['fig_dpi'])
         self.update_canvases()
 
     def display_image_stack(self, current_image):
