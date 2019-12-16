044258d4b7cf6ecdb48ad584e12ff2d28cce1671
Michael Smirnov <michael.smirnov@mpfi.org>
2019-12-16 14:30:33 -0500

    TimelineStepBlock finished writing tests

--- test/unit/test_timelineStepBlock.py
+++ test/unit/test_timelineStepBlock.py
@@ -6,6 +6,9 @@ from app.Timeline import TimelineStepBlock
 @pytest.fixture()
 def timeline_step_block():
     timeline_step_block = TimelineStepBlock()
+    timeline_step_block['start_time'] = 0
+    timeline_step_block['end_time'] = timeline_step_block['start_time'] + \
+                                      timeline_step_block['period'] * timeline_step_block['iterations']
     return timeline_step_block
 
 
@@ -17,9 +20,8 @@ def test_is_valid(timeline_step_block):
     assert timeline_step_block.is_valid()
 
 
-def test_shift_start_end_times():
-    pass
-
-
-def test_print_step_info():
-    pass
+def test_shift_start_end_times(timeline_step_block):
+    old_end_time = timeline_step_block['end_time']
+    timeline_step_block.shift_start_end_times(2)
+    assert timeline_step_block['start_time'] == 2
+    assert timeline_step_block['end_time'] == old_end_time + 2
