28782341128ed4ea8daa7a66175151ac7fe68bf5
Michael Smirnov <michael.smirnov@mpfi.org>
2019-12-16 11:56:53 -0500

    added test for timlinestepblock

---
+++ test/unit/test_timelineStepBlock.py
@@ -0,0 +1,20 @@
+import pytest
+
+from app.Timeline import TimelineStepBlock
+
+
+@pytest.fixture()
+def timeline_step_block():
+    timeline_step_block = TimelineStepBlock()
+    return timeline_step_block
+
+
+def test_is_valid(timeline_step_block):
+    assert timeline_step_block.is_valid()
+
+def test_shift_start_end_times():
+    pass
+
+
+def test_print_step_info():
+    pass
