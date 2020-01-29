524538f661d4bf913e80623d43b7165222756239
Michael Smirnov <michael.smirnov@mpfi.org>
2020-01-29 13:19:57 -0500

    added test to get coordinates from position

--- test/integration/test_positions.py
+++ test/integration/test_positions.py
@@ -5,6 +5,7 @@ from app.Coordinates import Coordinates
 from app.Position import Position
 from app.Positions import Positions
 
+
 @pytest.fixture()
 def coordinates():
     coordinates = Coordinates()
@@ -42,18 +43,26 @@ def positions(coordinates):
     ref_img = ReferenceImage()
     ref_img_zoomed_out = ReferenceImageZoomedOut()
     positions.create_new_pos(ref_img, ref_img_zoomed_out)
+    positions.create_new_pos(ref_img, ref_img_zoomed_out)
+    coordinates2 = coordinates.copy()
+    coordinates2.motor_x = 50
+    positions.set_coordinates(2, coordinates2)
     return positions
 
-def test_create_new_pos(positions):
-    assert positions.current_position == 1
 
-def test_move_to_pos_id():
-    # coordinates = self.positions.get_coordinates(pos_id)
-    # coordinates.update_to_center()
-    # self.communication.move_to_coordinates(coordinates)
-    assert False
+def test_set_coordinates(positions):
+    coordinates = positions.get_coordinates(2)
+    assert coordinates.motor_x == 50
+    assert coordinates.scan_voltage_x == 0
+
+
+def test_create_new_pos(positions):
+    assert positions.current_position == 2
+    assert positions[1]['coordinates'].motor_x == 0
+
 
 def test_get_coordinates(positions):
     pos_id = 1
     coordinates = positions.get_coordinates(pos_id)
     assert coordinates.motor_x == 0
+    assert coordinates.scan_voltage_x == 0
